f84664ea1ffa9b743f79f607e7f31f06147d835d
Carlos Sosa <carlosneftaly@gmail.com>
2021-07-25 18:06:13 -0500

    Cambios hasta el 25 jul 18:0

---
+++ src/scope/global.js
@@ -0,0 +1,17 @@
+var hello = 'Hello';
+let world = 'Hello World';
+const helloWorld = 'Hello World!';
+
+const anotherFunction = () => {
+    holaMundo = "Hola mundo!";
+};
+
+anotherFunction();
+
+const myFunction = () => {
+    var miVariable = holaMundo = "Holis";
+};
+
+myFunction();
+
+console.log(holaMundo);
